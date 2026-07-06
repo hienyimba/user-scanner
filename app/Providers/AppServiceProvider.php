4f8aee30ba1124856475e8e1305868725b247796
hienyimba <hienyimba@gmail.com>
2026-07-07 00:01:19 +0100

    Scanner Log Viewer - UUID

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        LogViewer::auth(function (Request $request): bool {
+            $routePath = trim((string) config('log-viewer.route_path', ''), '/');
+            $requestPath = trim($request->path(), '/');
+
+            return $routePath !== ''
+                && ($requestPath === $routePath || str_starts_with($requestPath, $routePath.'/'));
+        });
     }
 }
---
+++ config/log-viewer.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'route_path' => env('LOG_VIEWER_PATH', 'efa2822d-a607-4afe-a66e-59e90b667a86'),
+];
